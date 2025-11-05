5ac70604a8f7c326ba63c4e3d25eaef56a020f3d
BrunoRamosMejia <nelson.ramosmejia@gmail.com>
2025-11-05 20:28:23 -0300

    Base de Chat con Socket.io Funcional
    -Ya se pueden enviar y recibir mensajes por la app mediante Solicitudes Socket.io
    -Solo se puede acceder mediante Postman

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-// import { ChatGateway } from './chat.gateway';
-// import { ChatService } from './chat.service';
+import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessageEntity } from './entities/message.entity';
 
 @Module({
-  // providers: [ChatGateway, ChatService]
+  imports: [TypeOrmModule.forFeature([MessageEntity])],
+  providers: [ChatGateway, ChatService]
 })
 export class ChatModule {}
