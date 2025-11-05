50022a9781a1cbbaa22f727ba26f697613384e39
JhonatanCrruzz <jhonatancrruzz@gmail.com>
2025-11-05 23:10:44 +0100

    Actualización: ahora isCompleted se marca en true cuando el proveedor completa su registro
    Se modificó el método completeRegisterProvider para establecer provider.isCompleted = true una vez que el usuario finaliza el formulario de registro.
    
    Con este cambio, el sistema podrá identificar correctamente que el proveedor completó su información y redirigirlo al dashboard.
    
    No se modificaron otras partes del flujo (status sigue pasando de INCOMPLETE → ACTIVE).

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,21 +1,21 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { ChatService } from './chat.service';
-import { Socket } from 'socket.io';
-import { MessageEntity } from './entities/message.entity';
+// import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+// import { ChatService } from './chat.service';
+// import { Socket } from 'socket.io';
+// import { MessageEntity } from './entities/message.entity';
 
-@WebSocketGateway()
-export class ChatGateway {
-  constructor(private readonly chatService: ChatService) {}
+// @WebSocketGateway()
+// export class ChatGateway {
+//   constructor(private readonly chatService: ChatService) {}
 
-  @SubscribeMessage('sendMessage')
-  async handleMessage(
-    @MessageBody() data: { senderId: string, receiverId: string, content: string },
-    @ConnectedSocket() client: Socket,
-  ): Promise<MessageEntity> {
-    const message = await this.chatService.saveMessage(data);
+//   @SubscribeMessage('sendMessage')
+//   async handleMessage(
+//     @MessageBody() data: { senderId: string, receiverId: string, content: string },
+//     @ConnectedSocket() client: Socket,
+//   ): Promise<MessageEntity> {
+//     const message = await this.chatService.saveMessage(data);
 
-    client.broadcast.emit(`chat_${data.receiverId}`, message);
+//     client.broadcast.emit(`chat_${data.receiverId}`, message);
 
-    return message;
-  }
-}
+//     return message;
+//   }
+// }
