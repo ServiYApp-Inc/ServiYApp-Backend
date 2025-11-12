c7f48c9d8c2d9ef856ebb0aa3ea1360ddc44035b
BrunoRamosMejia <nelson.ramosmejia@gmail.com>
2025-11-12 01:05:14 -0300

    chat fix

--- src/modules/chat-control/chat-control.controller.ts
+++ src/modules/chat-control/chat-control.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ChatControlService } from './chat-control.service';
 import { ChatService } from '../chat/chat.service';
+import { MessageEntity } from '../chat/entities/message.entity';
+import { ChatGateway } from '../chat/chat.gateway';
 
 @Controller('chat-control')
 export class ChatControlController {
   constructor(
     private readonly chatControlService: ChatControlService,
     private readonly chatService: ChatService,
+    private readonly chatGateway: ChatGateway,
   ) {}
 
   @Get('messages-between')
