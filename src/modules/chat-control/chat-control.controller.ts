086dcae17737906e90cbc8885b2513e98c117ee4
BrunoRamosMejia <nelson.ramosmejia@gmail.com>
2025-11-12 14:49:15 -0300

    Chat Auth Update endpoint message-between

--- src/modules/chat-control/chat-control.controller.ts
+++ src/modules/chat-control/chat-control.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ChatControlService } from './chat-control.service';
 import { ChatService } from '../chat/chat.service';
 import { MessageEntity } from '../chat/entities/message.entity';
 import { ChatGateway } from '../chat/chat.gateway';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/roles.enum';
 
 @Controller('chat-control')
 export class ChatControlController {
@@ -12,7 +17,10 @@ export class ChatControlController {
     private readonly chatGateway: ChatGateway,
   ) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('messages-between')
+  @Roles(Role.Admin, Role.User, Role.Provider)
   async getMessagesBetween(
     @Query('senderId') senderId: string,
     @Query('receiverId') receiverId: string,
