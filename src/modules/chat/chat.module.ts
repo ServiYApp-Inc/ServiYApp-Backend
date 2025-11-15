f53c1b12fb049e7a2be0ce0a0ef041b43eb4e96a
AriadnaRamirez <ariadnamts98@gmail.com>
2025-11-15 13:39:52 -0600

    Chat con foto y nombre

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -4,9 +4,11 @@ import { MessageEntity } from './entities/message.entity';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
+import { User } from '../users/entities/user.entity';
+import { Provider } from '../providers/entities/provider.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessageEntity])],
+  imports: [TypeOrmModule.forFeature([MessageEntity, User, Provider])],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
   exports: [ChatService],
